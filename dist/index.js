ecf4e50d022d122ffb2cd5e0685cf3b39357b14c
ssuhacho <ssuhacho@gmail.com>
2022-05-19 15:15:31 -0500

    classes on typescript

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "Suha",
-    age: 27,
-    gender: "female"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("Lynn", 27, "female");
 const sayHi = (person) => {
     return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
 };
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
