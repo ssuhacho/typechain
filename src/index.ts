927c3bc35b4386f74706f9ebe81cc2296c2c5f3f
ssuhacho <ssuhacho@gmail.com>
2022-05-19 15:04:54 -0500

    pass an object using ts Interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-    return (`Hello ${name}, you are ${age}, you are a ${gender}!`);
+interface Human {
+    name: string,
+    age: number,
+    gender: string
+}
+
+const person = {
+    name: "Suha",
+    age: 27,
+    gender: "female"
 };
 
-console.log(sayHi("Suha",27,"female"));
+const sayHi = (person: Human):string => {
+    return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
+};
+
+console.log(sayHi(person));
 
 export{};
